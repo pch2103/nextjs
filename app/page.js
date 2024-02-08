99ed12b71afef9146c5a88a2c4e6231d5ed7391b
pch2103 <pch2103@gmail.com>
2024-02-08 20:15:09 +0300

    light theme

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by Pavel&nbsp;
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-sans text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-100 dark:border-neutral-800 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-blue-300 lg:p-4 ">
+          Get started by Pavel Cherenkov2&nbsp;
           <code className="font-mono font-bold">app/page.js</code>
         </p>
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Worked
+        <h1 className="mb-4 text-4xl font-normal leading-none tracking-tight text-gray-700 md:text-3xl lg:text-5xl :text-gray">
+          NextJS Worked
         </h1>
         </div>
     </main>
